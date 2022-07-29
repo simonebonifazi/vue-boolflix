de7ad2040d0c3a4d3d6ef7e0c9cd1f65746f0d81
simonebonifazi <simonbonifraz@gmail.com>
2022-07-29 12:41:27 +0200

    imported-axios

--- src/App.vue
+++ src/App.vue
@@ -6,22 +6,16 @@
 
 <script>
 
-
+import axios from 'axios'
 export default {
   name: 'boolflix',
   components: {
 
-  }
+  },
+  
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style >
+
 </style>
