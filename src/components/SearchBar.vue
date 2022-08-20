1c997f76490e48bde0bd0bb3e2b7cc943091d097
simonebonifazi <simonbonifraz@gmail.com>
2022-08-20 21:55:08 +0200

    added-searchbar

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <input type="text" :placeholder="placeholder" v-model.trim="term">
+    <button type="button" @click="$emit('search', term)"> 
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+name: "SearchBar",
+props:{
+    placeholder: {
+        type: String,
+        required: false,
+        default: "Cerca"
+    },
+    buttonText: {
+        type: String,
+        required:false,
+        default: "cerca"
+    }
+},
+data(){
+    return{
+        term: ""
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
