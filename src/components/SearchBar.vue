61b94bb301a731dce7006d47c1a70602ecb77f54
simonebonifazi <simonbonifraz@gmail.com>
2022-08-22 15:57:55 +0200

    fixed style button

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="d-flex align-items-center">
         <input type="text" :placeholder="placeholder" v-model.trim="term" class="form-control rounded mx-2">
-        <button type="button" class="btn btn-outline-danger rounded" @click="$emit('search', term)">
-            Cerca
+        <button type="button" class="btn btn-outline-danger rounded d-flex justify-content-center align-items-center" @click="$emit('search', term)">
+        Cerca
         </button>
     </div>
 </template>
